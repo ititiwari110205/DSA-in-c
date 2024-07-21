03c7c7aa1b0df629f2c183b2d750213d0f95d02c
iti tiwari <128047701+kanan110205@users.noreply.github.com>
2024-07-21 14:50:20 +0000

    Initial commit

---
+++ condition.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+  /*....................+ve,-ve and 0.....................
+  int a;
+ cout<<"value of A:";
+ cin>>a;
+ if(a>0){
+     cout<<"A is +ve";
+ }
+  else if (a<0){
+      cout<<"A is -ve";
+  }
+  else{
+      cout<<"A is 0";
+  }*/
+
+  /*................................max....................
+
+  int a,b;
+  cout<<"value of A:";
+  cin>>a;
+  cout<<"value of B:";
+  cin>>b;
+  if(a>b){
+      cout<<"A is greater";
+  }
+  else if (a<b){
+      cout<<"B is greater";
+  }
+      else{
+          cout<<"Both are equal";
+      }*/
+
+  return 0;
+}
